df7450b8d901e13e0196f13406fc88d7cc5c9f58
Faizal <jai.crys@gmail.com>
2021-04-10 02:03:55 +0800

    Metronic v8.0.0 release. Check changelog for more info: https://keenthemes.com/metronic/?page=changelog

---
+++ metronic_v8.0.0/theme/demo1/src/js/layout/app.js
@@ -0,0 +1,237 @@
+"use strict";
+
+// Class definition
+var KTApp = function() {
+    var _initPageLoader =  function() {
+        // CSS3 Transitions only after page load(.page-loading class added to body tag and remove with JS on page load)
+        KTUtil.removeClass(document.body, 'page-loading');
+    }
+
+    var _initBootstrapTooltips = function() {
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl);
+        });
+    }
+
+    var _initBootstrapPopovers = function() {
+        var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+
+        var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl);
+        });
+    }
+
+    var _initScrollSpy = function() {
+        var elements = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'));
+
+        elements.map(function (element) {
+            var sel = element.getAttribute('data-bs-target');
+            var offset = element.hasAttribute('data-bs-target-offset') ?  parseInt(element.getAttribute('data-bs-target-offset')) : 0;
+
+            KTUtil.on(document.body,  element.getAttribute('data-bs-target') + ' [href]', 'click', function(e) {
+                e.preventDefault();
+
+                var el = document.querySelector(this.getAttribute('href'));
+
+                KTUtil.scrollTo(el, offset);
+            });
+            
+            var scrollContent = document.querySelector(element.getAttribute('data-bs-target'));
+            var scrollSpy = bootstrap.ScrollSpy.getInstance(scrollContent);
+            if (scrollSpy) {
+                scrollSpy.refresh();
+            }
+        });
+    }
+
+    var _initButtons = function() {
+        var buttonsGroup = [].slice.call(document.querySelectorAll('[data-kt-buttons="true"]'));
+
+        buttonsGroup.map(function (group) {
+            var selector = group.hasAttribute('data-kt-buttons-target') ? group.getAttribute('data-kt-buttons-target') : '.btn';
+
+            // Toggle Handler
+            KTUtil.on(group, selector, 'click', function(e) {
+                var buttons = [].slice.call(group.querySelectorAll(selector + '.active'));
+
+                buttons.map(function (button) {
+                    button.classList.remove('active');
+                });
+
+                this.classList.add('active');
+            });
+        });
+    }   
+
+    var _initCheck = function() {
+        // Toggle Handler
+        KTUtil.on(document.body,  '[data-kt-check="true"]', 'change', function(e) {
+            var check = this;
+            var targets = document.querySelectorAll(check.getAttribute('data-kt-check-target'));
+
+            KTUtil.each(targets, function (target) {
+                if (target.type == 'checkbox') {
+                    target.checked = check.checked;
+                } else {
+                    target.classList.toggle('active');
+                }                
+            });
+        });
+    }
+
+    var _initSelect2 = function() {
+        var elements = [].slice.call(document.querySelectorAll('[data-control="select2"], [data-kt-select2="true"]'));
+       
+        elements.map(function (element) {
+            var options = {
+                dir: document.body.getAttribute('direction')
+            };
+
+            if ( element.getAttribute('data-hide-search') == 'true') {
+                options.minimumResultsForSearch = Infinity;
+            }
+            
+            $(element).select2(options);
+        });
+    }
+
+    var _initAutosize = function() {
+        var inputs = [].slice.call(document.querySelectorAll('[data-kt-autosize="true"]'));
+       
+        inputs.map(function (input) {
+            autosize(input);
+        });
+    }
+
+    var _countUp = function() {
+        var elements = [].slice.call(document.querySelectorAll('[data-kt-countup="true"]:not(.counted)'));
+
+        elements.map(function (element) {
+            if (KTUtil.isInViewport(element) && KTUtil.visible(element) ) {
+                var options = {};
+
+                var value = element.getAttribute('data-kt-countup-value');
+                value = parseFloat(value.replace(/,/,''));
+
+                if (element.hasAttribute('data-kt-countup-start-val')) {
+                    options.startVal = parseFloat(element.getAttribute('data-kt-countup-start-val'));
+                }
+
+                if (element.hasAttribute('data-kt-countup-duration')) {
+                    options.duration = parseInt(element.getAttribute('data-kt-countup-duration'));
+                }
+
+                if (element.hasAttribute('data-kt-countup-decimal-places')) {
+                    options.decimalPlaces = parseInt(element.getAttribute('data-kt-countup-decimal-places'));
+                }
+
+                if (element.hasAttribute('data-kt-countup-prefix')) {
+                    options.prefix = element.getAttribute('data-kt-countup-prefix');
+                }
+
+                if (element.hasAttribute('data-kt-countup-suffix')) {
+                    options.suffix = element.getAttribute('data-kt-countup-suffix');
+                }
+
+                var count = new countUp.CountUp(element, value, options);
+
+                count.start();
+                
+                element.classList.add('counted');
+            }                
+        });
+    }
+
+    var _initCountUp = function() {
+        // Initial call
+        _countUp();
+
+        // Window scroll event handler
+        window.addEventListener('scroll', _countUp);
+
+        // Tabs shown event handler
+        var tabs = [].slice.call(document.querySelectorAll('[data-kt-countup-tabs="true"][data-bs-toggle="tab"]'));
+        tabs.map(function (tab) {
+            tab.addEventListener('shown.bs.tab', _countUp);
+        });        
+    }
+
+    return {
+        init: function() {
+            this.initPageLoader();
+
+            this.initBootstrapTooltips();
+            
+            this.initBootstrapPopovers();
+            
+            this.initScrollSpy();
+            
+            this.initButtons();
+            
+            this.initCheck();
+            
+            this.initSelect2();
+            
+            this.initCountUp();
+
+            this.initAutosize();
+        },
+
+        initPageLoader: function() {
+            _initPageLoader();
+        },
+
+        initBootstrapTooltips: function() {
+            _initBootstrapTooltips();
+        },
+
+        initBootstrapPopovers: function() {
+            _initBootstrapPopovers();
+        },
+
+        initScrollSpy: function() {
+            _initScrollSpy();
+        },
+
+        initButtons: function() {
+            _initButtons();
+        },
+
+        initCheck: function() {
+            _initCheck();
+        },
+
+        initSelect2: function() {
+            _initSelect2();
+        },
+
+        countUp: function() {
+            _countUp();
+        },
+
+        initCountUp: function() {
+            _initCountUp();
+        },
+
+        initAutosize: function() {
+            _initAutosize();
+        }
+    };
+}();
+
+// On document ready
+KTUtil.onDOMContentLoaded(function() {
+    KTApp.init();
+});
+
+// On window load
+window.addEventListener("load", function() {
+	KTApp.initPageLoader();
+});
+
+// Webpack support
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = KTApp;
+}
