d508f99deb0ba9ab1a92c341893c26c10ba92894
Faizal <jai.crys@gmail.com>
2021-05-04 02:21:42 +0800

    Metronic v8.0.5 release. For more info check: https://keenthemes.com/metronic/?page=changelog

---
+++ metronic_v8.0.5/html_bootstrap5/theme/demo1/src/js/custom/landing.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Class definition
+var KTLandingPage = function () {
+    // Private methods
+    var initPortfolio = function(product) {
+        
+    }
+
+    // Public methods
+    return {
+        init: function () {
+            
+        }   
+    }
+}();
+
+// Webpack support
+if (typeof module !== 'undefined') {
+    module.exports = KTLandingPage;
+}
+
+// On document ready
+KTUtil.onDOMContentLoaded(function() {
+    KTLandingPage.init();
+});
